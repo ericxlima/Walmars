aa2f5f0d3097abe6bcd40d316dbba1b71365d306
Eric de Lima <58092119+ericxlima@users.noreply.github.com>
2020-11-28 10:12:45 -0300

    stable version

--- main.py
+++ main.py
@@ -95,72 +95,50 @@ class CardManagement:
         ]
         deposito_window = sg.Window('Depósito', deposito_layout, element_justification='center')
         while True:
-            button, values = deposito_window.read(close=True)
+            self.__data = {x[1]: x[-1] for x in self.__banco.load_cards()}
+            button, values = deposito_window.read()
             if button == 'ENVIAR':
                 if values['-NUMERO-'] == number:
-                    sg.popup()
-                self.__banco.transference(values['-VALOR-'], number, values['-NUMERO-'])
+                    sg.popup('Não é permitido trasferir para a mesma conta')
+                if values['-NUMERO-'] not in self.__data.keys():
+                    sg.popup('Este número não existe no nosso Banco de Dados')
+                if not self.luhn_implementacion(values['-NUMERO-']):
+                    sg.popup('Este número não pode ser um cartão de crédito')
+                if int(values['-VALOR-']) > int(self.__data[number]):
+                    sg.popup('Você não tem todo esse dinheiro')
+                else:
+                    self.__banco.transference(values['-VALOR-'], number, values['-NUMERO-'])
+                    sg.popup('Dinheiro tranferido com sucesso')
+                deposito_window.close()
+                self.__administrar_conta(number)
+            if button in ('VOLTAR', None):
+                deposito_window.close()
+                self.__administrar_conta(number)
 
     def __administrar_conta(self, numero):
-
-
-
-
-
-        self.__data = {x[1]: x[-1] for x in self.__data}
         logado_layout = [
             [sg.Text('Escolha uma opção: ')],
             [sg.B('SALDO'), sg.B('DEPÓSITO'), sg.B('TRANSFERIR'), sg.B('FECHAR CONTA'), sg.B('SAIR')]
         ]
         logado_window = sg.Window('Administrar conta', logado_layout, element_justification='center')
         while True:
-            button, values = logado_window.read(close=True)
+            self.__data = {x[1]: x[-1] for x in self.__banco.load_cards()}
+            button, values = logado_window.read()
             if button == 'SALDO':
                 sg.popup('Seu saldo é de {}'.format(self.__data[numero]), title='Saldo')
             if button == 'DEPÓSITO':
                 value = sg.popup_get_text('Insira o quanto você quer depositar:', title='Depósito')
                 self.__banco.add_balance(value, numero)
+            if button == 'TRANSFERIR':
+                logado_window.close()
+                self.__transferencia(numero)
+            if button == 'FECHAR CONTA':
+                self.__banco.delete_account(numero)
+                sg.popup('Conta Fechada com Sucesso')
+                logado_window.close()
             if button in (None, 'SAIR'):
+                logado_window.close()
                 self.__front_page()
 
 
-
-
-def luhn_implementacion(s):
-    return s
-
-
-def login(user_number, banco):
-    choice2 = input('1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n0. Exit\n')
-    while choice2 != '5':
-        if choice2 == '1':
-            print([x[-1] for x in banco.load_cards() if user_number in x][0])
-        if choice2 == '2':
-            income = int(input('\nEnter income:\n'))
-            banco.update_balance(income, user_number)
-            print('Income was added!')
-
-        if choice2 == '3':
-            transfer_card = input('\nTransfer\nEnter card number:\n')
-            cards = {x[1]: x[-1] for x in banco.load_cards()}
-            if not luhn_implementacion(transfer_card):
-                print('Probably you made a mistake in the card number. Please try again!\n')
-            elif transfer_card not in cards.keys():
-                print('Such a card does not exist.\n')
-            else:
-                transfer_money = int(input('\nEnter how much money you want to transfer:\n'))
-                if transfer_money > int(cards[user_number]):
-                    print('Not enough money!')
-                else:
-                    banco.transference(transfer_money, user_number, transfer_card)
-                    print('Success')
-
-        if choice2 == '4':
-            banco.delete_account(user_number)
-
-        if choice2 == '0':
-            return True  # para encerrar o programa futuramente
-        choice2 = input('1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n0. Exit\n')
-
-
-test = CardManagement()
+run = CardManagement()
